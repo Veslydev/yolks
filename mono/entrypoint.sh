3f29474f37c84aeca60c519e9d7afb20b4529cd8
Vesly <74131882+Veslydev@users.noreply.github.com>
2025-11-07 19:48:11 +0300

    Load environment variables from .env file
    Added functionality to load environment variables from a .env file if it exists, enhancing configuration management.

--- mono/entrypoint.sh
+++ mono/entrypoint.sh
@@ -2,17 +2,20 @@
 # Wait for the container to fully initialize
 sleep 1
 
+# Load environment variables from .env file if it exists
+if [ -f /home/container/.env ]; then
+    echo -e "Loading environment variables from .env file"
+    export $(grep -v '^#' /home/container/.env | xargs)
+fi
+
 # Default the TZ environment variable to UTC.
 TZ=${TZ:-UTC}
 export TZ
-
 # Set environment variable that holds the Internal Docker IP
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
-
 # Switch to the container's working directory
 cd /home/container || exit 1
-
 if [ ! -z "${SRCDS_APPID}" ]; then
     ## just in case someone removed the defaults.
     if [ "${STEAM_USER}" == "" ]; then
@@ -36,16 +39,12 @@ if [ ! -z "${SRCDS_APPID}" ]; then
         else
             echo -e "No appid set. Starting Server"
         fi
-
     else
         echo -e "Not updating game server as auto update was set to 0. Starting Server"
     fi
 fi
-
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
-
 # Run the Server
 eval ${MODIFIED_STARTUP}
